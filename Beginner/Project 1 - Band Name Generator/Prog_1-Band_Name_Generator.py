c44a9c2c53859f1574bc1075f17cab28e8ae45be
a-kori <115870233+a-kori@users.noreply.github.com>
2022-10-20 21:22:04 +0200

    Add files via upload

---
+++ Beginner/Project 1 - Band Name Generator/Prog_1-Band_Name_Generator.py
@@ -0,0 +1,8 @@
+print("Welcome to the Band Name Generator!")
+
+hometown = input("What's the name of your hometown?\n")
+pet_name = input("What's the name of your first pet?\n")
+
+#print("Your new band name is: " + hometown + " " + pet_name + "!")
+#print("Your new band name is: {} {}!".format(hometown, pet_name))
+print(f"Your new band name is: {hometown} {pet_name}!")
