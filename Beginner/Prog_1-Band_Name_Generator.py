3f635d9cccfbce314d5b0e59ab560c6a14529d84
a-kori <115870233+a-kori@users.noreply.github.com>
2022-10-20 21:23:33 +0200

    Delete Prog_1-Band_Name_Generator.py

--- Beginner/Prog_1-Band_Name_Generator.py
+++
@@ -1,8 +0,0 @@
-print("Welcome to the Band Name Generator!")
-
-hometown = input("What's the name of your hometown?\n")
-pet_name = input("What's the name of your first pet?\n")
-
-#print("Your new band name is: " + hometown + " " + pet_name + "!")
-#print("Your new band name is: {} {}!".format(hometown, pet_name))
-print(f"Your new band name is: {hometown} {pet_name}!")
