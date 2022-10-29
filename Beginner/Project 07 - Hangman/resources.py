7f2b555dfc44bc3405ee765ffc95e44783db2bc2
a-kori <115870233+a-kori@users.noreply.github.com>
2022-10-29 13:53:03 +0200

    Update resources.py

--- Beginner/Project 07 - Hangman/resources.py
+++ Beginner/Project 07 - Hangman/resources.py
@@ -1,11 +1,13 @@
 import os
 clear = lambda: os.system('cls')
 
+file = open("Project 7 - Hangman\english-dictionary.txt")
 word_list = []
-for word in open("Project 7 - Hangman\english-dictionary.txt") :
+for word in file :
     new_word = word.rstrip("'s\n").lower()
     if len(new_word) > 2 :
         word_list.append(new_word)
+file.close()
 
 hangman_logo = '''
  _                                             
