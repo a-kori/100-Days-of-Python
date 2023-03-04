08a751d150fc0869f4dc49191d479096e9a54782
a-kori <nastya.korzhylova@gmail.com>
2023-03-04 18:25:28 +0100

    Adding documentation to Mail Merge.

--- Intermediate/Project 24 - Mail Merge/main.py
+++ Intermediate/Project 24 - Mail Merge/main.py
@@ -1,9 +1,14 @@
 import os
+
+# Save the project's absolute path as PROJECT_PATH
 PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 
+# Read the invitation draft from starting_letter.txt
 with open(PROJECT_PATH + "/Input/Letters/starting_letter.txt") as darft:
     invitation = darft.read()
 
+# For each name in invited_names.txt, create a new file and write
+# the invitation in it, replacing [name] with each name accordingly
 with open(PROJECT_PATH + "/Input/Names/invited_names.txt") as names:
     for name in names.readlines():
         name = name.strip()
