d40db2537dfac3b83dc3d87c886ae3ac7f08d127
anastasiia.korzhylova <anastasiia.korzhylova@tu-dortmund.de>
2023-03-04 16:35:29 +0100

    Adding high score record to Snake.

--- Intermediate/Project 21 - Snake Game/scoreboard.py
+++ Intermediate/Project 21 - Snake Game/scoreboard.py
@@ -1,7 +1,9 @@
 from turtle import Turtle
+import os
+HIGH_SCORE_PATH = os.path.dirname(os.path.abspath(__file__)) + "\high_score.txt"
 
 class Scoreboard(Turtle):
-    '''A class displaying and keeping track of the player's score in the Snake Game.'''
+    '''A class displaying and keeping track of the player's (high) score in the Snake Game.'''
 
     def __init__(self) -> None:
         '''Initializes the score with 0 and displays it on the top of the screen.'''
@@ -17,9 +19,26 @@ class Scoreboard(Turtle):
         '''Increments the score and displays the change on the screen.'''
         self.score += 1
         self.clear()
-        self.write(f"Score: {self.score}", align="center", font=('Consolas', 12, 'normal'))
+        self.write(f"Score: {self.score}, High Score: {self.get_high_score()}", 
+                   align="center", font=('Consolas', 12, 'normal'))
 
     def end_game(self) -> None:
-        '''Displays the 'Game over' sign in the center of the screen.'''
+        '''Displays the 'Game over' sign in the center of the screen and updates the high score.'''
+        if self.score > self.get_high_score():
+            self.update_high_score(self.score)
+        
         self.setposition(0, 0)
         self.write(f"GAME OVER", align="center", font=('Consolas', 12, 'normal'))
+
+    def get_high_score(self) -> int:
+        '''Returns the current high score of the game.'''
+        file = open(HIGH_SCORE_PATH, 'r')
+        high_score = int(file.readline())
+        file.close()
+        return high_score
+
+    def update_high_score(self, new_high_score : int) -> None:
+        '''Overwrites the current high score of the game.'''
+        file = open(HIGH_SCORE_PATH, 'w')
+        file.write(str(new_high_score))
+        file.close()
